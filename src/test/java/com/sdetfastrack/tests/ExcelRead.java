9b8f6ea794a731286a51c00f68c070f1aaa9ced4
pakdil <eaasbb@gmail.com>
2022-11-30 22:11:09 +0300

    SdetFastTrack ExcelRead.java

--- src/test/java/com/sdetfastrack/tests/ExcelRead.java
+++ src/test/java/com/sdetfastrack/tests/ExcelRead.java
@@ -33,7 +33,41 @@ public class ExcelRead {
     // <3> Select row and cell
     // Print out mary's cell
     // Indexes start from 0
+    System.out.println(sheet.getRow(1).getCell(1));
+
+    // Print out Developer
+
     System.out.println(sheet.getRow(3).getCell(2));
+
+    // Return the count of used cells only
+    // Starts counting from 1
+    int usedRows = sheet.getPhysicalNumberOfRows();
+    System.out.println(usedRows);
+
+    //Returns the number from top cell to bottom cell
+    // It dosen't care if the cell empty or not
+    // Starts counting from 0
+
+    int lastUsedRow = sheet.getLastRowNum();
+    System.out.println(lastUsedRow);
+
+    // TODO: Create a logic to print Vinod's name
+    for (int rowNum = 0; rowNum<usedRows; rowNum++) {
+
+        if (sheet.getRow(rowNum).getCell(0).toString().equals("Vinod")) {
+            System.out.println(sheet.getRow(rowNum).getCell(0));
+        }
+    }
+
+    //TODO: Create a logic to print out Linda's Job_ID
+    //Check if name is Linda --> print out Job_ID of Linda
+    for (int rowNum = 0; rowNum < usedRows; rowNum++) {
+
+        if (sheet.getRow(rowNum).getCell(0).toString().equals("Linda")) {
+            System.out.println(sheet.getRow(rowNum).getCell(2));
+        }
+    }
+
 
 }
 
