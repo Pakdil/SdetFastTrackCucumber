042ecc67c06c9338cde22dd338ba81a7f2d6e9af
pakdil <eaasbb@gmail.com>
2022-12-02 10:34:07 +0300

    SdetFastTrack VyTrackDashboardPage

---
+++ src/test/java/com/sdetfastrack/pages/VyTrackDashboardPage.java
@@ -0,0 +1,24 @@
+package com.sdetfastrack.pages;
+
+import com.sdetfastrack.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class VyTrackDashboardPage {
+
+    public VyTrackDashboardPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//li[@id='user-menu']/a")
+    public WebElement fullName;
+
+    @FindBy(linkText = "Logout")
+    public WebElement logOutLink;
+
+    public void logOut() {
+        fullName.click();
+        logOutLink.click();
+    }
+}
