888437544d157a9ce38c52f9b1d1c328dce63279
pakdil <eaasbb@gmail.com>
2022-12-02 11:22:19 +0300

    SdetFastTrack DDTTest WebDriverWait

--- src/test/java/com/sdetfastrack/tests/VrTrackDDTTest.java
+++ src/test/java/com/sdetfastrack/tests/VrTrackDDTTest.java
@@ -9,6 +9,9 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -37,9 +40,21 @@ public class VrTrackDDTTest {
         XSSFWorkbook workbook = new XSSFWorkbook(in);
         XSSFSheet sheet = workbook.getSheet("data");
 
+        for (int i = 0; i < sheet.getLastRowNum(); i++) {
+
+            String userName = sheet.getRow(i).getCell(0).toString();
+            String passWord = sheet.getRow(i).getCell(1).toString();
+            String firstName = sheet.getRow(i).getCell(2).toString();
+            String lastName = sheet.getRow(i).getCell(3).toString();
+
+            loginPage.login(userName, passWord);
+
+            WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 30);
+            wait.until(ExpectedConditions.visibilityOf(dashboardPage.fullName));
 
 
-
+        }
+        
 
     }
 }
